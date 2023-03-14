443b840683769f11ba3e3116742212c4d72eb0a5
Frank Dixon <frankdixoneverything@gmail.com>
2023-03-13 20:17:33 -0600

    Completed implementation with tests

--- main.js
+++ main.js
@@ -1,5 +1,39 @@
 (function(w) {
     w.addEventListener('message', (event) => {
-        console.log('Received a message from origin', event.origin);
+        const permittedDomains = [
+            'events.humanitix.com',
+            'events-staging.humanitix.net',
+            'events-experiments.humanitix.net'
+        ];
+
+        // Ensure we only respond to emitted events from known Humanitix pages
+        if (permittedDomains.indexOf(event.origin) < 0) {
+            return;
+        }
+
+        // Ensure we only respond to analytics echo events
+        if (
+            typeof event.data !== "object" ||
+            (
+                event.data.type !== 'hx-datalayer-echo' &&
+                event.data.type !== 'hx-metapixel-echo'
+            ) ||
+            typeof event.data.contents === 'undefined') {
+            return;
+        }
+
+        // Detect which analytics integrations are available
+        const dataLayerIsAvailable = typeof window.dataLayer !== 'undefined';
+        const metaPixelIsAvailable = typeof window.fbq !== 'undefined';
+
+        // Respond to Data Layer echo events
+        if (dataLayerIsAvailable && event.data.type === 'hx-datalayer-echo') {
+            dataLayer.push(event.data.contents);
+        }
+
+        // Respond to Meta Pixel echo events
+        if (metaPixelIsAvailable && event.data.type === 'hx-metapixel-echo') {
+            fbq(...event.data.contents);
+        }
     });
 })(window);
--- main.test.js
+++ main.test.js
@@ -14,14 +14,38 @@ beforeEach(() => {
     });
 });
 
-test('it logs message to console when event is dispatched', async () => {
-    console.log = jest.fn();
+test('it echos data layer events', async () => {
+    window.dataLayer = {
+        push: jest.fn()
+    };
 
     require('./main.js');
 
-    window.postMessage('This is a test message', '*');
+    const message = {
+        type: 'hx-datalayer-echo',
+        contents: 'Example data layer contents'
+    };
+
+    window.postMessage(message, '*');
 
     await new Promise(resolve => setTimeout(resolve, 100));
 
-    expect(console.log).toBeCalledWith('Received a message from origin', 'events.humanitix.com');
+    expect(window.dataLayer.push).toBeCalledWith('Example data layer contents');
+});
+
+test('it echos meta pixel events', async () => {
+    window.fbq = jest.fn();
+
+    require('./main.js');
+
+    const message = {
+        type: 'hx-metapixel-echo',
+        contents: ['fbq param 1', 'fbq param 2', 'fbq param 3']
+    };
+
+    window.postMessage(message, '*');
+
+    await new Promise(resolve => setTimeout(resolve, 100));
+
+    expect(window.fbq).toBeCalledWith('fbq param 1', 'fbq param 2', 'fbq param 3');
 });
